4b617de49cc2ec7144a96b6103bd0c98929bfa71
Avinash <kumaravi651@gmail.com>
2018-12-05 01:33:48 +0530

    Committed First Simple Python Programme

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print(1+2)
+print(7+6)
+print()
+
+greeting = "Hello"
+name = input("PlEASE ENTER YOUR NAME") #Its Like Scan f
+print(greeting + " " + name)
+
+splintString = "This string has been\nsplit over\nseveral\nlines"
+print(splintString)
+
+tabbedString = "1\t2\t3\t5\t6"
+print(tabbedString)
---
+++ HelloWorld/Variables.py
@@ -0,0 +1,11 @@
+greeting = "Bruce"
+_myName = "Tim"
+Tim = "Good"
+Tim_Was_57 = "Hello"
+Greeting = "There"
+print(Tim_Was_57 + ' ' + greeting)
+
+age = 24
+print(age)
+
+print(greeting + ' is ' + str(age) + ' Years Old')
