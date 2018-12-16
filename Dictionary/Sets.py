07e8276f47554fc48ab69650c90d526c4cfe66ff
avi651 <kumaravi651@gmail.com>
2018-12-16 23:45:31 +0530

    Add files via upload

---
+++ Dictionary/Sets.py
@@ -0,0 +1,13 @@
+farm_animals = {"sheep","cow","hen"}
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+print("*" * 40)
+
+wild_animals = set(["Lion","Tiger","Panther","Elephant","Hare"])
+print(wild_animals)
+
+farm_animals.add("horse")
+farm_animals.add("horse")
---
+++ Modules/Turtledemo.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+done = "Well done, you have finished your drawing"
+
+# noinspection PyUnrolvedRefrence
+#turtle.forward(150)
+#turtle.right(250)
+#turtle.circle()
+forward(150)
+right(250)
+forward(150)
+circle(75)
+
+#time.sleep(4)
+done()
+print(done)
